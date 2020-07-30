77e0f6f6b8cde3cc4b149ba9e53d8141bedf83ef
jaysvishwagit <jaysvishwa@gmail.com>
2020-07-30 11:36:46 +0530

    local

---
+++ app/Jobs/MailSendJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\TestMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class MailSendJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to('jaysvishwa@gmail.com')->send(new TestMail);
+    }
+}
--- routes/test.php
+++ routes/test.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Jobs\TestJob;
+use App\Mail\TestMail;
+use App\Jobs\MailSendJob;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/mail',function(){
-
+    dispatch(new MailSendJob);
+    return 'Mail Sent successfully';
 });
 
 Route::get('/job',function(){
