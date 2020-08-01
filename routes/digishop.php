1ce35af51130c9759948cc94f64bd5a3df952ec5
jaysvishwagit <jaysvishwa@gmail.com>
2020-08-01 16:20:22 +0530

    local

--- routes/digishop.php
+++ routes/digishop.php
@@ -7,11 +7,11 @@ Route::get('/products/category/{category}','Client\DigiShopController@category_p
 
 Route::group(['prefix' => 'cart','middleware'=>['auth']],function(){
 
-    Route::get('/','Client\DigiShop\CartController@cart')->name('cart');
-    Route::get('/add/{product}','Client\DigiShop\CartController@add_to_cart')->name('cart.item.add');
-    Route::get('/delete/{productid}','Client\DigiShop\CartController@delete_item_from_cart')->name('cart.item.delete');
-    Route::get('/delete','Client\DigiShop\CartController@delete_cart')->name('cart.delete');
-    Route::get('/checkout','Client\DigiShop\CartController@checkout')->name('cart.checkout');
+    Route::get('/','Client\digishop\CartController@cart')->name('cart');
+    Route::get('/add/{product}','Client\digishop\CartController@add_to_cart')->name('cart.item.add');
+    Route::get('/delete/{productid}','Client\digishop\CartController@delete_item_from_cart')->name('cart.item.delete');
+    Route::get('/delete','Client\digishop\CartController@delete_cart')->name('cart.delete');
+    Route::get('/checkout','Client\digishop\CartController@checkout')->name('cart.checkout');
 
 });
 
