993cc520912470c18ca78467c02e87312caec54e
jaysvishwagit <jaysvishwa@gmail.com>
2020-08-14 12:20:56 +0530

    local

--- database/migrations/2020_08_10_152716_create_orders_table.php
+++ database/migrations/2020_08_10_152716_create_orders_table.php
@@ -15,6 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('order_id');
             $table->integer('user_id')->unsigned();
             $table->text('cart');
             $table->string('payment_gateway')->nullable();
